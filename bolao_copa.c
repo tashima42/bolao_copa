00b2370093ecc6eed75a8da7d8f4e93ea7da39a6
Pedro Tashima <tithe.coup0l@icloud.com>
2026-06-28 18:19:14 -0300

    remove attribution and logs

--- bolao_copa.c
+++ bolao_copa.c
@@ -1,30 +1,3 @@
-/*-
- * SPDX-License-Identifier: BSD-2-Clause
- *
- * Copyright (c) 2026 Brian J. Downs
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
 #include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
@@ -500,7 +473,8 @@ int main(void) {
   pthread_mutex_init(&state.mutex, NULL);
 
   const char *csv_path = getenv("ANSWERS_CSV");
-  if (csv_path == NULL) csv_path = "answers.csv";
+  if (csv_path == NULL)
+    csv_path = "answers.csv";
   state.file = fopen(csv_path, "a");
   if (state.file == NULL) {
     fprintf(stderr, "failed to open %s\n", csv_path);
@@ -530,10 +504,6 @@ int main(void) {
 
   printf("Server starting on:\n");
   printf("  HTTP:      http://%s:%d\n", config.host, config.port);
-
-  printf("Run\n\ncurl http://%s:%d/\n", config.host, config.port);
-  printf("curl http://%s:%d/api/hello\n", config.host, config.port);
-  printf("curl http://%s:%d/user/alice\n\n", config.host, config.port);
 
   // start server (blocking)
   if (papago_start(server, &config) != 0) {
